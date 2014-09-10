e0bb2231df5728361d1adae5f2a37b07e15549b6
Tim <tim@timcollins.ie>
2014-09-10 19:53:26 +0100

    Added initial version of bubble sort.

---
+++ src/Sorting.Algorithms/BubbleSort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sorting.Algorithms
+{
+    public static class BubbleSort
+    {
+        public static int[] Sort(int[] source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (source.Length == 0 || source.Length == 1)
+            {
+                return source;
+            }
+
+            return source;
+        }
+    }
+}
---
+++ src/Sorting.UnitTests/BubbleSortTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sorting.UnitTests
+{
+    class BubbleSortTests
+    {
+        [Test]
+        public void NullSourceThrowsException()
+        {
+            int[] source = null;
+
+            Assert.Throws<ArgumentNullException>(() => Algorithms.BubbleSort.Sort(source));
+        }
+
+        [Test]
+        public void EmptySourceReturnsEmptyResult()
+        {
+            int[] source = { };
+            var result = Algorithms.BubbleSort.Sort(source);
+
+            Assert.AreEqual(source, result);
+        }
+
+        [Test]
+        public void SingleElementSourceReturnsSingleElement()
+        {
+            int[] source = { 2 };
+
+            var result = Algorithms.BubbleSort.Sort(source);
+
+            Assert.AreEqual(source, result);
+        }
+    }
+}
